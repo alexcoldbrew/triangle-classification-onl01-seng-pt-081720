1713316a3d355de84142062cbeddcc23bda9b944
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-22 21:32:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
       raise TriangleError
       
-    elsif 
+    elsif side_1 + side_2 <= side_3
     
     else
       if side_1 == side_2 && side_1 == side_3
